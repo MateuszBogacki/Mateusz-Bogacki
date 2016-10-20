0ca1b2d96cfad12942a06bf53cec95ab0248beba
MateuszBogacki <manti.2@vp.pl>
2016-10-20 21:25:36 +0200

    first commit

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 12;
+            Console.BackgroundColor = ConsoleColor.DarkBlue;
+            Console.WriteLine("C# xD! Hello World!");
+        }
+    }
+}
